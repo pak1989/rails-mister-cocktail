c6e77b75ba14a4b185e3701b30ad308db498ab33
Paolo Kuzminsky <paolok89@gmail.com>
2018-02-22 15:32:53 +0000

    all greens thats incredible

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,35 @@
+require 'byebug'
+class DosesController < ApplicationController
+  before_action :set_dose, only: [:destroy]
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    if @dose.save
+      # byebug
+      redirect_to @dose.cocktail
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose.destroy
+    redirect_to cocktails_url
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_dose
+      @dose = Dose.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def dose_params
+      params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
+  # get 'doses/new'
+
+  # get 'doses/create'
+
+  # get 'doses/destroy'
+
+  # get 'cocktails/index'
+
+  # get 'cocktails/show'
+
+  # get 'cocktails/new'
+
+  # get 'cocktails/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get "doses/:id", to: "doses#show", as: :dose
+  delete "doses/:id", to: "doses#destroy"
+
+  resources :cocktails, only: [ :index, :show, :new, :create] do
+    resources :doses, only: [ :new, :create ]
+  end
 end
